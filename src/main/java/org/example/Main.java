48667aaa23465c17305f99903401392b034ef876
michaelcast01 <michael2003cc@gmail.com>
2025-02-19 12:03:42 -0500

    cambios2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        system.out.printl("hola case");
+        system.out.printl("hola clase");
         system.out.printl("estamos aprendiendo comandos");
         system.out.printl("repasando comandos");
         system.out.printl("DDL");
