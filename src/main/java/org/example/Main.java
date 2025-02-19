3b03abe08832776bef5e1a9b87db4799df00fe8c
michaelcast01 <michael2003cc@gmail.com>
2025-02-19 11:24:39 -0500

    cambios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
+        system.out.printl("hola case");
+        system.out.printl("estamos aprendiendo comandos");
+        system.out.printl("repasando comandos");
     }
 }
